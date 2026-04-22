b7bbeb50294b07191d62cc1435f8748f1a3a38da
LucasTheMaker <msntbn6n8p@privaterelay.appleid.com>
2026-04-21 22:12:56 -0700

    Add album data for 'Ye' and 'Thriller'

--- albums.js
+++ albums.js
@@ -1,1 +1,22 @@
+window.albums = [
+  {
+    id: "ye",
+    title: "Ye",
+    artist: "Kanye West",
+    cover: "images/ye.jpg",
+    tracks: [
+      { number: 1, title: "I Thought About Killing You", file: "1.mp3" },
+      { number: 2, title: "Yikes", file: "2.mp3" }
+    ]
+  },
 
+  {
+    id: "thriller",
+    title: "Thriller",
+    artist: "Michael Jackson",
+    cover: "images/thriller.jpg",
+    tracks: [
+      { number: 1, title: "Thriller", file: "thriller.mp3" }
+    ]
+  }
+];
