207be81f8dbd9e2c396f241da2edf30006a18986
LucasTheMaker <msntbn6n8p@privaterelay.appleid.com>
2026-04-21 22:14:35 -0700

    Fix track listings for albums in albums.js

--- albums.js
+++ albums.js
@@ -6,7 +6,12 @@ window.albums = [
     cover: "images/ye.jpg",
     tracks: [
       { number: 1, title: "I Thought About Killing You", file: "1.mp3" },
-      { number: 2, title: "Yikes", file: "2.mp3" }
+      { number: 2, title: "Yikes", file: "2.mp3" },
+      { number: 3, title: "All Mine", file: "3.mp3" },
+      { number: 4, title: "Wouldn't Leave", file: "4.mp3" },
+      { number: 5, title: "No Mistakes", file: "5.mp3" },
+      { number: 6, title: "Ghost Town", file: "6.mp3" },
+      { number: 7, title: "Violent Crimes", file: "7.mp3" }
     ]
   },
 
@@ -16,7 +21,9 @@ window.albums = [
     artist: "Michael Jackson",
     cover: "images/thriller.jpg",
     tracks: [
-      { number: 1, title: "Thriller", file: "thriller.mp3" }
+      { number: 1, title: "Wanna Be Startin Somethin", file: "1.mp3" },
+      { number: 2, title: "Baby Be Mine", file: "2.mp3" },
+      { number: 3, title: "Thriller", file: "3.mp3" }
     ]
   }
 ];
